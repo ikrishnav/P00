a76f7d6ad4efb7479f3bd93c9644006098b31275
krishnavijayan <krishnavijayan@localhost>
2023-05-25 14:04:57 +0800

    first push to P00 repository in GitHbu

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -1,55 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    @Test
-    public void testAdd() {
-        int a = 4321;
-        int b = 1234;
+	@Before
+	public void setUp() throws Exception {
+	}
 
-        Calculator cal = new Calculator();
-        int actual = cal.add(a, b);
+	@After
+	public void tearDown() throws Exception {
+	}
 
-        int expected = 5555;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSubtract() {
-        int a = 5000;
-        int b = 2000;
-
-        Calculator cal = new Calculator();
-        int actual = cal.subtract(a, b);
-
-        int expected = 3000;
-        assertEquals(expected, actual);
-    }
-
-    @Test 
-    public void testMultiply() {
-        int a = 5;
-        int b = 10;
-
-        Calculator cal = new Calculator();
-        int actual = cal.multiply(a, b);
-
-        int expected = 50;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testDivide() {
-        int a = 20;
-        int b = 5;
-
-        Calculator cal = new Calculator();
-        double actual = cal.divide(a, b);
-
-        double expected = 4.0;
-        assertEquals(expected, actual, 0.001);
-    }
-
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
